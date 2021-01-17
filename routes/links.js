b6ace666d270cf89ceb1530006b047227e43f69b
Boris Martínez <borisbam@hotmail.com>
2021-01-17 00:25:44 -0300

    Deploy

---
+++ routes/links.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const linksController = require('../controllers/linksController');
+const { check } = require('express-validator');
+const auth = require('../middleware/auth');
+
+router.post('/',
+    [
+        check('name', 'Upload a file').not().isEmpty(),
+        check('original_name', 'Name the file').not().isEmpty()
+        
+    ],
+    auth,
+    linksController.newLink,
+
+);
+
+router.get('/',
+    linksController.allLinks
+)
+
+router.get('/:url',
+    linksController.hasPassword,
+    linksController.takeLink
+)
+
+router.post('/:url',
+    linksController.verifyPassword,
+    linksController.takeLink
+)
+
+module.exports = router;
